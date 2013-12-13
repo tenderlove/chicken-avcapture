f67ec51ac8b75306a196295b1e2631000473f88c
Aaron Patterson <aaron.patterson@gmail.com>
2013-12-13 14:30:56 -0800

    initial commit

---
+++ av_capture.scm
@@ -0,0 +1,10 @@
+;;;; av_capture.scm
+;;;; Bindings to AVCapture
+
+(module av-capture
+  (aaron)
+
+(import scheme chicken)
+
+(define (aaron) "hello")
+)
---
+++ tests/run.scm
@@ -0,0 +1,8 @@
+(use av-capture test srfi-1)
+
+(test-begin "av-capture")
+
+(test "aaron" "aaron" (aaron))
+
+(test-end)
+(test-exit)
